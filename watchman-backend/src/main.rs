dda829ac5ad1ba9b1a77a30a378daa184badae61
cdog-shen <shencdog@outlook.com>
2024-11-17 00:48:42 +0800

    change: OK to serve basic API

--- watchman-backend/src/main.rs
+++ watchman-backend/src/main.rs
@@ -19,15 +19,20 @@ use share_lib;
 use share_lib::cfg_reader::GLOBAL_CONFIG_HANDLER;
 
 // local modules
+mod api;
+mod config;
 mod middleware;
 
 #[actix_rt::main]
-async fn main() {
+async fn main() -> io::Result<()> {
     // config out put
     println!("config is {:#?}", &*GLOBAL_CONFIG_HANDLER);
 
     // init logger
-    // share_lib::logger::init_logger(&*GLOBAL_CONFIG_HANDLER.server_config.log_level);
+    share_lib::logger::init_logger(
+        &GLOBAL_CONFIG_HANDLER.server_config.log_path,
+        &*GLOBAL_CONFIG_HANDLER.server_config.log_level,
+    );
 
     // init mysql connection pool
     let manager =
@@ -35,6 +40,13 @@ async fn main() {
     let pool = diesel::r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.");
+
+    // setting listen addr
+    let app_url = format!(
+        "{}:{}",
+        &GLOBAL_CONFIG_HANDLER.server_config.listen_addr,
+        &GLOBAL_CONFIG_HANDLER.server_config.listen_port
+    );
 
     HttpServer::new(move || {
         App::new()
@@ -50,10 +62,10 @@ async fn main() {
             )
             .app_data(web::Data::new(pool.clone()))
             // wrap custom looger from share-lib logger
-            .wrap(actix_web::middleware::Logger::default())
+            // .wrap(middleware::log_middleware::logger)
             // Comment this line if you want to integrate with yew-address-book-frontend
-            .wrap(crate::middleware::auth_middleware::Authentication)
-            .wrap_fn(|req, srv| srv.call(req).map(|res| res))
+            // .wrap(crate::middleware::auth_middleware::Authentication)
+            // .wrap_fn(|req, srv| srv.call(req).map(|res| res))
             .configure(config::app::config_services)
     })
     .bind(&app_url)?
