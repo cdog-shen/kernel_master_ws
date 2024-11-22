af422610110750b7edbc6c4d6391a487b9c0398a
cdog-shen <shencdog@outlook.com>
2024-11-22 17:29:44 +0800

    add: UserToken  generate function

--- watchman-backend/src/models/user_token.rs
+++ watchman-backend/src/models/user_token.rs
@@ -1,16 +1,37 @@
 use jsonwebtoken::{EncodingKey, Header};
 use log::debug;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use share_lib::log_debug;
+
+use crate::models;
 
 static EXP_CONST: i64 = 60 * 60 * 24 * 7; // in seconds
 
 #[derive(Serialize, Deserialize)]
 pub struct UserToken {
-    // issued at
-    pub iat: i64,
-    // expiration
-    pub exp: i64,
-    // data
     pub user: String,
-    pub login_session: String,
+    pub uuid: String,
+    pub exp: i64, // expiration
 }
+
+// impl UserToken {
+//     pub fn generate_token(login: &models::user::UserModule) -> String {
+//         log_debug!("Token Max Age: {}", EXP_CONST);
+
+//         let now = Utc::now().timestamp_nanos() / 1_000_000_000; // nanosecond -> second
+//         let payload = UserToken {
+//             exp: now + EXP_CONST,
+//             user: login.user.clone(),
+//             uuid: Uuid::new_v4().to_string(),
+//         };
+
+//         jsonwebtoken::encode(
+//             &Header::default(),
+//             &payload,
+//             &EncodingKey::from_secret(&KEY),
+//         )
+//         .unwrap()
+//     }
+// }
