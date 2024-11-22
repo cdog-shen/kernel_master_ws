cbe22296c3c92e8713cead096dd81b9d106b44ee
cdog-shen <shencdog@outlook.com>
2024-11-22 19:45:00 +0800

    add: user signup logic (NOT FINISH)

--- watchman-backend/src/models/user.rs
+++ watchman-backend/src/models/user.rs
@@ -1,6 +1,11 @@
-use diesel::Queryable;
+use core::hash;
+
+use bcrypt::hash;
+use diesel::{prelude::*, Identifiable, Insertable, MysqlConnection, Queryablem};
 use serde::{Deserialize, Serialize};
 use time;
+
+use crate::config;
 
 #[derive(Queryable, Debug, Serialize, Deserialize)]
 pub struct UserModule {
@@ -13,4 +18,36 @@ pub struct UserModule {
     pub group: String,
     pub date_joined: time::PrimitiveDateTime,
     pub last_login: Option<time::PrimitiveDateTime>,
+}
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[diesel(table_name = users)]
+pub struct SignUpDataStream {
+    pub user: String,
+    pub passwd: String,
+}
+
+pub struct UserDataStream {}
+
+pub struct AuthDataStream {}
+
+impl UserModule {
+    pub fn signup(
+        new_user: SignUpDataStream,
+        conn: &mut MysqlConnection,
+    ) -> Result<String, String> {
+        if Self::find_user_by_username(&new_user.user, conn).is_err() {
+            let new_user = SignUpDataStream {
+                passwd: new_user.passwd,
+                ..new_user
+            };
+            diesel::insert_into(users).values(new_user).execute(conn);
+            Ok(constants::MESSAGE_SIGNUP_SUCCESS.to_string())
+        } else {
+            Err(format!(
+                "User '{}' is already registered",
+                &new_user.username
+            ))
+        }
+    }
 }
