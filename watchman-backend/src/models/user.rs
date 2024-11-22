8368e5642ae8374f6af7cdfd884f89faedbc0872
cdog-shen <shencdog@outlook.com>
2024-11-22 16:21:51 +0800

    change: redesign UserModule

--- watchman-backend/src/models/user.rs
+++ watchman-backend/src/models/user.rs
@@ -5,15 +5,12 @@ use time;
 #[derive(Queryable, Debug, Serialize, Deserialize)]
 pub struct UserModule {
     pub id: i32,
-    pub password: String,
-    pub username: String,
-    pub group: String,
-    pub is_superuser: bool,
-    pub email: String,
-    pub is_staff: bool,
-    pub is_active: bool,
+    pub user: String,
+    pub passwd: String,
+    pub is_enable: bool,
     pub name: String,
-    pub phone: Option<String>,
+    pub contact: String,
+    pub group: String,
     pub date_joined: time::PrimitiveDateTime,
     pub last_login: Option<time::PrimitiveDateTime>,
 }
