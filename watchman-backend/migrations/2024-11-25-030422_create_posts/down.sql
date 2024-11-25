bc1ab73372440fd7b67c2bc99bc25915590f1308
cdog-shen <shencdog@outlook.com>
2024-11-25 12:08:19 +0800

    del: remove useless migration file

--- watchman-backend/migrations/2024-11-25-030422_create_posts/down.sql
+++
@@ -1,7 +0,0 @@
--- This file should undo anything in `up.sql`
-
-
-
-ALTER TABLE `user` DROP COLUMN `groups`;
-ALTER TABLE `user` ADD COLUMN `groups` SET(5);
-
--- watchman-backend/migrations/2024-11-25-030422_create_posts/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-
-
-
-ALTER TABLE `user` DROP COLUMN `groups`;
-ALTER TABLE `user` ADD COLUMN `groups` USERGROUPSSET(5);
-
