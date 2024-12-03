0890a1cd41b7071e6b0ab05c9944087b88fe6444
cdog-shen <shencdog@outlook.com>
2024-12-04 01:21:32 +0800

    change: JWT generate function adjustment

--- watchman-backend/src/models/user_token.rs
+++ watchman-backend/src/models/user_token.rs
@@ -1,11 +1,12 @@
-use jsonwebtoken::{EncodingKey, Header};
+use chrono::{DateTime, Utc};
+use jsonwebtoken::{decode, encode, EncodingKey, Header, Validation};
 use log::debug;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use share_lib::log_debug;
+use share_lib::cfg_reader::SECRET_KEY;
 
-use crate::models;
+use crate::models::user::BasicUserDataStream;
 
 static EXP_CONST: i64 = 60 * 60 * 24 * 7; // in seconds
 
@@ -16,22 +17,34 @@ pub struct UserToken {
     pub exp: i64, // expiration
 }
 
-// impl UserToken {
-//     pub fn generate_token(login: &models::user::UserModule) -> String {
-//         log_debug!("Token Max Age: {}", EXP_CONST);
+impl UserToken {
+    pub fn encode_token(
+        login_user: String,
+    ) -> Result<String, jsonwebtoken::errors::Error> {
+        debug!("Token Max Age: {}", EXP_CONST);
 
-//         let now = Utc::now().timestamp_nanos() / 1_000_000_000; // nanosecond -> second
-//         let payload = UserToken {
-//             exp: now + EXP_CONST,
-//             user: login.user.clone(),
-//             uuid: Uuid::new_v4().to_string(),
-//         };
+        let now = Utc::now();
+        let exp = now.timestamp() + EXP_CONST; // Convert to Unix timestamp
+        let payload = UserToken {
+            exp,
+            user: login_user,
+            uuid: Uuid::new_v4().to_string(),
+        };
 
-//         jsonwebtoken::encode(
-//             &Header::default(),
-//             &payload,
-//             &EncodingKey::from_secret(&KEY),
-//         )
-//         .unwrap()
-//     }
-// }
+        encode(
+            &Header::default(),
+            &payload,
+            &EncodingKey::from_secret(&*SECRET_KEY.as_bytes()),
+        )
+    }
+
+    // pub fn decode_token(token: &str) -> Result<UserToken, jsonwebtoken::errors::Error> {
+    //     let decoding_key = EncodingKey::from_secret(&*SECRET_KEY.as_bytes());
+    //     let decoded: UserToken = decode::<UserToken>(
+    //         token,
+    //         &decoding_key,
+    //         &Validation::new(jsonwebtoken::Algorithm::HS256),
+    //     )?;
+    //     Ok(decoded)
+    // }
+}
