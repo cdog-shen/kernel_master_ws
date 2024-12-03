a1bca0ce303f3a34e152b1d7d2808862d8066da6
cdog-shen <shencdog@outlook.com>
2024-12-03 18:04:26 +0800

    remove unused test code

--- share-lib/src/lib.rs
+++ share-lib/src/lib.rs
@@ -1,13 +1,3 @@
 pub mod cfg_reader;
 pub mod data_structure;
 pub mod logger;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
-    }
-}
