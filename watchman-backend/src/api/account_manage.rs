b2c9205569daa4c7f06370d8396521365aa654ba
cdog-shen <shencdog@outlook.com>
2024-12-04 14:39:48 +0800

    add: mapping MailManErr to MailManErrResponser

--- watchman-backend/src/api/account_manage.rs
+++ watchman-backend/src/api/account_manage.rs
@@ -5,10 +5,9 @@ use diesel::{
 };
 
 use crate::utils::err_mapping::MailManErrResponser;
-use share_lib::data_structure::MailManErr;
 
 use crate::{
-    models::user::BasicUserDataStream, services::account_service, utils::err_mapping::MapErrorKey,
+    models::user::BasicUserDataStream, services::account_service,
 };
 
 // POST api/auth/login
@@ -18,6 +17,6 @@ pub async fn login(
 ) -> Result<HttpResponse, MailManErrResponser> {
     match account_service::login(login_json.0, &pool) {
         Ok(token_res) => Ok(HttpResponse::Ok().json(token_res)),
-        Err(err_mm) => Err(MailManErrResponser::mapping_from_mme(err_mm)),
+        Err(err_mm) => Err(MailManErrResponser { mme_msg: err_mm.msg }),
     }
 }
--- watchman-backend/src/services/account_service.rs
+++ watchman-backend/src/services/account_service.rs
@@ -29,7 +29,7 @@ pub fn login<'a>(
             return Err(MailManErr::new(
                 400,
                 "Bad Request",
-                format!("Bad user name or passwd for {}", user.username),
+                user.username,
                 1,
             ));
         }
