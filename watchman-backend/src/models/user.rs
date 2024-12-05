513dfee9d1ad7fd775113623ad74251b0305c2c7
cdog-shen <shencdog@outlook.com>
2024-12-05 11:17:12 +0800

    add: update_last_login function OKEY to use

--- watchman-backend/src/models/user.rs
+++ watchman-backend/src/models/user.rs
@@ -1,16 +1,10 @@
-use core::hash;
-
-use bcrypt::hash;
 use chrono;
-use diesel::{prelude::*, Identifiable, Insertable, MysqlConnection, Queryable, Selectable};
+use diesel::{prelude::*, Insertable, MysqlConnection, Queryable, Selectable};
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    config,
-    models::schema::user::{self, dsl::*},
-};
+use crate::models::schema::user::{self, dsl::*};
 
-#[derive(Queryable, Selectable, Debug, Serialize, Deserialize)]
+#[derive(Queryable, Selectable, Debug, Serialize, Deserialize, Insertable)]
 #[diesel(table_name = user)]
 pub struct UserModule {
     pub id: u32,
@@ -24,7 +18,7 @@ pub struct UserModule {
     pub last_login: Option<chrono::NaiveDateTime>,
 }
 
-#[derive(Queryable, Selectable, Debug, Serialize, Deserialize)]
+#[derive(Queryable, Selectable, Debug, Serialize, Deserialize, Insertable)]
 #[diesel(table_name = user)]
 pub struct UserInfo {
     pub id: u32,
@@ -44,9 +38,9 @@ pub struct BasicUserDataStream {
     pub passwd: String,
 }
 
-pub struct UserDataStream {}
+// pub struct UserDataStream {}
 
-pub struct AuthDataStream {}
+// pub struct AuthDataStream {}
 
 impl UserModule {
     pub fn get_user_by_username(user_name: &str, conn: &mut MysqlConnection) -> Option<UserInfo> {
@@ -60,6 +54,7 @@ impl UserModule {
             Err(_) => None,
         }
     }
+
     pub fn get_user_info(conn: &mut MysqlConnection) -> Vec<UserInfo> {
         match user
             .select(UserInfo::as_select()) // 选择 UserInfo 结构体中定义的字段
@@ -70,7 +65,10 @@ impl UserModule {
         }
     }
 
-    pub fn login(user_data: &BasicUserDataStream, conn: &mut MysqlConnection) -> Option<UserInfo> {
+    pub fn login(
+        user_data: &BasicUserDataStream,
+        conn: &mut MysqlConnection,
+    ) -> Result<UserInfo, String> {
         match user
             .filter(is_enable.eq(1))
             .filter(username.eq(&user_data.username))
@@ -78,8 +76,28 @@ impl UserModule {
             .select(UserInfo::as_select())
             .get_result::<UserInfo>(conn)
         {
-            Ok(user_identified) => Some(user_identified),
-            Err(_) => None,
+            Ok(user_identified) => Ok(user_identified),
+            Err(err) => Err(format!("Login filed: {} - {}", &user_data.username, err.to_string())),
+        }
+    }
+
+    pub fn update_last_login(
+        user_data: &BasicUserDataStream,
+        conn: &mut MysqlConnection,
+    ) -> Result<String, String> {
+        match Self::get_user_by_username(&user_data.username, conn) {
+            Some(user_line) => {
+                if diesel::update(user.find(user_line.id))
+                    .set(last_login.eq(chrono::Utc::now().naive_utc()))
+                    .execute(conn)
+                    .is_err()
+                {
+                    Err(format!("{}'s last_login update filed.", &user_line.username))
+                } else {
+                    Ok(format!("{}'s token update DONE.", &user_line.username))
+                }
+            }
+            None => Err(format!("can NOT find user {}", &user_data.username)),
         }
     }
 }
