6fc041ff00e5d832c48623740f56e1da89f4405f
cdog-shen <shencdog@outlook.com>
2024-12-05 12:55:55 +0800

    add: Completed the implementation of token ORM and atomized the operations.

--- watchman-backend/src/models/user_token.rs
+++ watchman-backend/src/models/user_token.rs
@@ -1,14 +1,88 @@
-use chrono::{DateTime, Utc};
-use jsonwebtoken::{decode, encode, EncodingKey, Header, Validation};
+use core::num;
+
+use actix_web::body::None;
+use chrono::Utc;
+use diesel::{prelude::*, Insertable, MysqlConnection, Queryable, Selectable};
+use jsonwebtoken::{encode, EncodingKey, Header};
 use log::debug;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use share_lib::cfg_reader::SECRET_KEY;
 
-use crate::models::user::BasicUserDataStream;
+use crate::models::schema::token::{self, dsl::*};
 
 static EXP_CONST: i64 = 60 * 60 * 24 * 7; // in seconds
+
+#[derive(Queryable, Selectable, Debug, Serialize, Deserialize, Insertable)]
+#[diesel(table_name = token)]
+pub struct TokenModel {
+    pub tokenid: String,
+    pub username: String,
+    pub exp_time: chrono::NaiveDateTime,
+}
+
+impl<'a> TokenModel {
+    pub fn find_token_by_username(
+        user_token: &UserToken,
+        conn: &mut MysqlConnection,
+    ) -> Result<TokenModel, String> {
+        match token
+            .filter(username.eq(&user_token.user))
+            .get_result::<TokenModel>(conn)
+        {
+            Ok(token_line) => Ok(token_line),
+            Err(_) => Err(format!("can NOT find {}'s token.", &user_token.user)),
+        }
+    }
+
+    pub fn update_token(
+        user_token: &UserToken,
+        conn: &mut MysqlConnection,
+    ) -> Result<String, String> {
+        match Self::find_token_by_username(&user_token, conn) {
+            Ok(token_line) => {
+                match diesel::update(token.find(token_line.username))
+                    .set((
+                        tokenid.eq(user_token.user.to_string()),
+                        exp_time.eq(chrono::NaiveDateTime::from_timestamp(
+                            user_token.exp.clone(),
+                            0,
+                        )),
+                    ))
+                    .execute(conn)
+                {
+                    Ok(num_of_change) => Ok(format!(
+                        "{}'s token update. lines: {}",
+                        &user_token.user, num_of_change
+                    )),
+                    Err(err) => Err(err.to_string()),
+                }
+            }
+            Err(msg) => Err(msg),
+        }
+    }
+
+    pub fn insert_new_token(
+        user_token: &UserToken,
+        conn: &mut MysqlConnection,
+    ) -> Result<String, String> {
+        match diesel::insert_into(token)
+            .values(TokenModel {
+                tokenid: user_token.uuid.clone(),
+                username: user_token.user.clone(),
+                exp_time: chrono::NaiveDateTime::from_timestamp(user_token.exp.clone(), 0),
+            })
+            .execute(conn)
+        {
+            Ok(num_of_change) => Ok(format!(
+                "{}'s token updated. line: {}",
+                &user_token.user, num_of_change
+            )),
+            Err(err) => Err(err.to_string()),
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct UserToken {
