433922535254f3e8932cbe1dc7c0d32141316e3d
cdog-shen <shencdog@outlook.com>
2024-12-09 19:16:38 +0800

    add: access model

---
+++ watchman-backend/src/models/access.rs
@@ -0,0 +1,197 @@
+use chrono::{self, Local};
+use diesel::{
+    prelude::*, result::Error::NotFound, Insertable, MysqlConnection, Queryable, Selectable,
+};
+use serde::{Deserialize, Serialize};
+
+use crate::models::schema::access_table::{self, dsl::*};
+
+static NOT_FOUND_CODE: u8 = 1;
+static TMI_ERROR_CODE: u8 = 2;
+static UNKNOW_ERROR_CODE: u8 = 0;
+
+/// The structure of the Access stored in the database.
+/// - access_id
+///
+///     Access Id (u32)
+///
+/// - service_id
+///
+///     service id from service table (u32)
+///
+///  - group_id
+///
+///     group id from group table (u32)
+///
+///  - group access
+///
+///     group access for is service (u8 tinyint)
+///
+/// - is_enable
+///
+///    is this group enable (tinyint 1/0)
+///
+/// - create_time
+///
+///     create time (datetime %Y-%m-%d %H:%M:%S)
+///
+#[derive(Queryable, Selectable, Debug, Serialize, Deserialize, Insertable)]
+#[diesel(table_name = access_table)]
+pub struct AccessModel {
+    pub id: u32,
+    #[diesel(column_name = service_id)]
+    pub service_id: u32,
+    #[diesel(column_name = group_id)]
+    pub group_id: u32,
+    #[diesel(column_name = group_access)]
+    pub group_access: u8,
+    #[diesel(column_name = is_enable)]
+    pub is_enable: u8,
+    #[diesel(column_name = update_time)]
+    pub update_time: Option<chrono::NaiveDateTime>,
+}
+
+#[derive(Queryable, Selectable, Debug, Serialize, Deserialize, Insertable, AsChangeset)]
+#[diesel(table_name = access_table)]
+pub struct AccessInputStream {
+    pub id: Option<u32>,
+    pub service_id: Option<u32>,
+    pub group_id: Option<u32>,
+    pub group_access: Option<u8>,
+    pub is_enable: Option<u8>,
+    pub update_time: Option<chrono::NaiveDateTime>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AccessOutputStream {
+    pub id: Option<u32>,
+    pub service_id: Option<u32>,
+    pub group_id: Option<u32>,
+    pub group_access: Option<u8>,
+    pub is_enable: Option<u8>,
+    pub update_time: Option<chrono::NaiveDateTime>,
+}
+
+fn map_model_to_output_stream(access_info: AccessModel) -> AccessOutputStream {
+    AccessOutputStream {
+        id: Some(access_info.id),
+        service_id: Some(access_info.service_id),
+        group_id: Some(access_info.group_id),
+        group_access: Some(access_info.group_access),
+        is_enable: Some(access_info.is_enable),
+        update_time: access_info.update_time,
+    }
+}
+
+// query implement
+impl AccessModel {
+    /// get all access
+    pub fn get_all(conn: &mut MysqlConnection) -> Result<Vec<AccessOutputStream>, (u8, String)> {
+        match access_table
+            .select(AccessModel::as_select())
+            .get_results::<AccessModel>(conn)
+        {
+            Ok(access_table_data) => {
+                let group_output_stream_data: Vec<AccessOutputStream> = access_table_data
+                    .into_iter()
+                    .map(|access_info| map_model_to_output_stream(access_info))
+                    .collect();
+                Ok(group_output_stream_data)
+            }
+            Err(e) => Err((
+                UNKNOW_ERROR_CODE,
+                format!("Unknow Error: {}.", e.to_string()),
+            )),
+        }
+    }
+
+    /// get all enable access
+    pub fn get_all_enable(
+        conn: &mut MysqlConnection,
+    ) -> Result<Vec<AccessOutputStream>, (u8, String)> {
+        match access_table
+            .filter(is_enable.eq(1))
+            .select(AccessModel::as_select())
+            .get_results::<AccessModel>(conn)
+        {
+            Ok(access_table_data) => {
+                let group_output_stream_data: Vec<AccessOutputStream> = access_table_data
+                    .into_iter()
+                    .map(|access_info| map_model_to_output_stream(access_info))
+                    .collect();
+                Ok(group_output_stream_data)
+            }
+            Err(e) => Err((
+                UNKNOW_ERROR_CODE,
+                format!("Unknow Error: {}.", e.to_string()),
+            )),
+        }
+    }
+}
+
+// update implement
+impl AccessModel {
+    pub fn new_access(
+        access_info: &AccessInputStream,
+        conn: &mut MysqlConnection,
+    ) -> Result<String, (u8, String)> {
+        match diesel::insert_into(access_table)
+            .values((
+                service_id.eq(access_info.service_id.unwrap()),
+                group_id.eq(access_info.group_id.unwrap()),
+                group_access.eq(access_info.group_access.unwrap()),
+                is_enable.eq(access_info.is_enable.unwrap_or(0)),
+                update_time.eq(Local::now().naive_local()),
+            ))
+            .execute(conn)
+        {
+            Ok(num_of_change) => Ok(format!(
+                "Access {} created. line: {}",
+                &access_info.service_id.clone().unwrap(),
+                num_of_change
+            )),
+            Err(err) => Err((UNKNOW_ERROR_CODE, err.to_string())),
+        }
+    }
+
+    pub fn update_access_by_id(
+        access_info: &AccessInputStream,
+        conn: &mut MysqlConnection,
+    ) -> Result<String, (u8, String)> {
+        match diesel::update(access_table.find(access_info.id.unwrap()))
+            .set(access_info)
+            .execute(conn)
+        {
+            Ok(num_of_eff) => match num_of_eff {
+                0 => Err((
+                    NOT_FOUND_CODE,
+                    format!("id: {} not found", access_info.id.unwrap()),
+                )),
+                1 => Ok(format!(
+                    "{}'s data updated. lines: {}",
+                    access_info.id.unwrap(),
+                    num_of_eff
+                )),
+                _ => Err((
+                    TMI_ERROR_CODE,
+                    format!("id: {} Too much info", access_info.id.unwrap()),
+                )),
+            },
+            Err(e) => Err((UNKNOW_ERROR_CODE, e.to_string())),
+        }
+    }
+
+    pub fn delete_access_by_id(
+        access_id: u32,
+        conn: &mut MysqlConnection,
+    ) -> Result<String, (u8, String)> {
+        match diesel::delete(access_table.find(access_id)).execute(conn) {
+            Ok(num_of_eff) => Ok(format!(
+                "{}'s data deleted. lines: {}",
+                access_id, num_of_eff
+            )),
+            Err(NotFound) => Err((NOT_FOUND_CODE, format!("id: {} not found", access_id))),
+            Err(e) => Err((UNKNOW_ERROR_CODE, e.to_string())),
+        }
+    }
+}
