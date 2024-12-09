63f512aa025078fc5648098634af7a40d9735a25
cdog-shen <shencdog@outlook.com>
2024-12-09 19:17:56 +0800

    add: register mods in project

--- watchman-backend/src/api/mod.rs
+++ watchman-backend/src/api/mod.rs
@@ -2,3 +2,4 @@ pub mod hey_hi_hello;
 pub mod account_manage;
 pub mod group_manage;
 pub mod service_manage;
+pub mod access_manage;
--- watchman-backend/src/services/mod.rs
+++ watchman-backend/src/services/mod.rs
@@ -1,2 +1,4 @@
 pub mod account_service;
 pub mod access_service;
+pub mod service_service;
+pub mod group_service;
