837003e343602e69a5bde10dd473cda311d183f1
cdog-shen <shencdog@outlook.com>
2024-12-09 19:18:31 +0800

    add: register service and access route

--- watchman-backend/src/config/app.rs
+++ watchman-backend/src/config/app.rs
@@ -19,18 +19,29 @@ pub fn config_services(cfg: &mut web::ServiceConfig) {
                     .service(web::resource("/login").route(web::post().to(account_manage::login)))
                     .service(web::resource("/logout").route(web::post().to(account_manage::logout)))
                     .service(web::resource("/user_update").route(web::post().to(account_manage::user_update))) 
-                    //         .service(
-                    //             web::resource("/{id}")
-                    //                 .route(web::get().to(address_book_controller::find_by_id))
-                    //                 .route(web::put().to(address_book_controller::update))
-                    //                 .route(web::delete().to(address_book_controller::delete)),
             )
-            .service(web::scope("/access_control")
+            .service(web::scope("/group_control")
                 .service(web::resource("/all_group").route(web::get().to(group_manage::all_group)))
                 .service(web::resource("/new_group").route(web::post().to(group_manage::new_group)))
                 .service(web::resource("/update_group").route(web::post().to(group_manage::update_group)))
                 .service(web::resource("/delete_group").route(web::delete().to(group_manage::delete_group)))
-
-            ),
-    );
+            )
+            .service(web::scope("/service_control")
+                .service(web::resource("/all_service").route(web::get().to(service_manage::all_service)))
+                .service(web::resource("/new_service").route(web::post().to(service_manage::new_service)))
+                .service(web::resource("/update_service").route(web::post().to(service_manage::update_service)))
+                .service(web::resource("/delete_service").route(web::delete().to(service_manage::delete_service)))
+            )
+            .service(web::scope("/access_control")
+                .service(web::resource("/all_access").route(web::get().to(access_manage::all_access)))
+                .service(web::resource("/new_access").route(web::post().to(access_manage::new_access)))
+                .service(web::resource("/update_access").route(web::post().to(access_manage::update_access)))
+                .service(web::resource("/delete_access").route(web::delete().to(access_manage::delete_access)))
+            )
+        );
 }
+
+
+//         .service(
+//             web::resource("/{id}")
+//                 .route(web::request_type().to(manage::fn)),
