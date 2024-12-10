dc02760f84373d5ff8960e79fd6f34e5354584bb
cdog-shen <shencdog@outlook.com>
2024-12-11 00:34:34 +0800

    add: add DB migrate command

--- watchman-backend/install.sh
+++ watchman-backend/install.sh
@@ -1,6 +1,8 @@
 # watchman install script
 
+# var set
 INSTALL_LOC=$1
+DATABASE_URL=$2
 
 
 
@@ -12,7 +14,9 @@ mkdir logs
 
 
 # init RSA keys
-
 openssl genpkey -algorithm RSA -out $INSTALL_LOC/keys/jwt_pri.pem
 openssl rsa -pubout -in $INSTALL_LOC/keys/jwt_pri.pem -out $INSTALL_LOC/keys/jwt_pub.pem
 
+# init DB tables
+diesel migration generate --database-url $DATABASE_URL init_migration --diff-schema
+diesel migration run --database-url $DATABASE_URL
