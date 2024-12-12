754feff062ede9be2c3ff6ae23ceed66fb4f5d49
cdog-shen <shencdog@outlook.com>
2024-12-12 19:02:46 +0800

    add: add InputJsonStruct for deserialization

--- watchman-backend/src/api/account_manage.rs
+++ watchman-backend/src/api/account_manage.rs
@@ -3,14 +3,40 @@ use diesel::{
     r2d2::{ConnectionManager, Pool},
     MysqlConnection,
 };
+use serde::{Deserialize, Serialize};
 
 use crate::{
-    utils::err_mapping::MailManErrResponser,
-    models::user::*,
+    models::user::{self, *},
     services::account_service,
+    utils::err_mapping::MailManErrResponser,
 };
 
-// GET api/auth/all
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InputJsonStruct {
+    pub id: Option<u32>,
+    pub username: Option<String>,
+    pub passwd: Option<String>,
+    pub is_enable: Option<u8>,
+    pub name: Option<String>,
+    pub contact: Option<serde_json::Value>,
+    pub date_joined: Option<chrono::NaiveDateTime>,
+    pub last_login: Option<chrono::NaiveDateTime>,
+}
+
+fn deserialization_input_json_struct(input: InputJsonStruct) -> UserInputStream {
+    UserInputStream {
+        id: input.id,
+        username: input.username,
+        passwd: input.passwd,
+        is_enable: input.is_enable,
+        name: input.name,
+        contact: Some(serde_json::to_string(&input.contact.unwrap()).unwrap()),
+        date_joined: None,
+        last_login: None,
+    }
+}
+
+// GET api/auth/all_user
 pub async fn get_all(
     pool: web::Data<Pool<ConnectionManager<MysqlConnection>>>,
 ) -> Result<HttpResponse, MailManErrResponser> {
@@ -19,7 +45,6 @@ pub async fn get_all(
         Err(err_mm) => Err(MailManErrResponser::mapping_from_mme(err_mm)),
     }
 }
-
 
 // POST api/auth/login
 pub async fn login(
@@ -56,10 +81,10 @@ pub async fn logout(
 
 // POST api/auth/user_update
 pub async fn user_update(
-    user_info: web::Json<UserInputStream>,
+    user_info: web::Json<InputJsonStruct>,
     pool: web::Data<Pool<ConnectionManager<MysqlConnection>>>,
 ) -> Result<HttpResponse, MailManErrResponser> {
-    match account_service::user_update(user_info.0, &pool) {
+    match account_service::user_update(deserialization_input_json_struct(user_info.0), &pool) {
         Ok(token_res) => Ok(HttpResponse::Ok().json(token_res)),
         Err(err_mm) => Err(MailManErrResponser::mapping_from_mme(err_mm)),
     }
