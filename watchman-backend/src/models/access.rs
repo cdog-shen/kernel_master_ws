aac5625b1b0f8e56a639b58f94909535577f13ce
cdog-shen <shencdog@outlook.com>
2024-12-14 14:43:14 +0800

    add: add new query function

--- watchman-backend/src/models/access.rs
+++ watchman-backend/src/models/access.rs
@@ -154,6 +154,27 @@ impl AccessModel {
         }
     }
 
+    pub fn get_access_by_sids(
+        sid_list: Vec<u32>,
+        conn: &mut MysqlConnection,
+    ) -> Result<Vec<AccessOutputStream>, (u8, String)> {
+        match access_table
+            .filter(is_enable.eq(1))
+            .filter(service_id.eq_any(sid_list))
+            .select(AccessModel::as_select())
+            .get_results::<AccessModel>(conn)
+        {
+            Ok(access_table_data) => Ok(access_table_data
+                .into_iter()
+                .map(|access_info| map_model_to_output_stream(access_info))
+                .collect()),
+            Err(e) => Err((
+                UNKNOW_ERROR_CODE,
+                format!("Unknow Error: {}.", e.to_string()),
+            )),
+        }
+    }
+
     /// get user's max access
     pub fn get_max_permission(
         gid_list: Vec<u32>,
