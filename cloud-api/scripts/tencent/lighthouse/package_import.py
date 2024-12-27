281d498ab066857331bf92015152a0abbaa448d0
cdog-shen <shencdog@outlook.com>
2024-12-27 17:13:28 +0800

    add: implement Tencent Cloud Lighthouse instance description script

---
+++ cloud-api/scripts/tencent/lighthouse/package_import.py
@@ -0,0 +1,7 @@
+import json
+import types
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.lighthouse.v20200324 import lighthouse_client, models
