b564543b6442b0d1dc15f57bdf37a4e1a2efca21
cdog-shen <shencdog@outlook.com>
2025-03-03 14:29:11 +0800

    add: redesigned script structure has been

--- cloud-api/scripts/tencent/lighthouse/tencent_lighthouse_describeinstances.py
+++ cloud-api/scripts/tencent/lighthouse/tencent_lighthouse_describeinstances.py
@@ -4,35 +4,32 @@
 # sk: SecretKEY
 # region: 地区
 
-from package_import import *
+import json
+import types
+from package_import import ApiClient_Lighthouse, TencentCloudSDKException
+import sys
 
 
 try:
-    # 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
-    # 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
-    # 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
-    cred = credential.Credential(
-        "AK", "SK"
-    )
-    # 实例化一个http选项，可选的，没有特殊需求可以跳过
-    httpProfile = HttpProfile()
-    httpProfile.endpoint = "lighthouse.tencentcloudapi.com"
+    if len(sys.argv) != 5:
+        print("Usage: python tencent_lighthouse_describeinstances.py <AK> <SK> <region> <params>")
+        sys.exit(1)
 
-    # 实例化一个client选项，可选的，没有特殊需求可以跳过
-    clientProfile = ClientProfile()
-    clientProfile.httpProfile = httpProfile
-    # 实例化要请求产品的client对象,clientProfile是可选的
-    client = lighthouse_client.LighthouseClient(cred, "ap-beijing", clientProfile)
+    AK = sys.argv[1]
+    SK = sys.argv[2]
+    region = sys.argv[3]
+    params = json.loads(sys.argv[4])
+    endpoint = "lighthouse.tencentcloudapi.com"
 
-    # 实例化一个请求对象,每个接口都会对应一个request对象
-    req = models.DescribeInstancesRequest()
-    params = {}
-    req.from_json_string(json.dumps(params))
+    client = ApiClient_Lighthouse(AK, SK, endpoint, region)
 
-    # 返回的resp是一个DescribeInstancesResponse的实例，与请求对象对应
-    resp = client.DescribeInstances(req)
-    # 输出json格式的字符串回包
+    req = client.ModelsHandler().DescribeInstancesRequest()
+    req.from_json_string(jsonStr=json.dumps(params))
+
+    resp = client.ClientHandler().DescribeInstances(req)
+
     print(resp.to_json_string())
+
 
 except TencentCloudSDKException as err:
     print(err)
