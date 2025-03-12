cffde500687cf659d35b0ccbfc24b905ed577d80
cdog-shen <shencdog@outlook.com>
2025-03-12 15:05:46 +0800

    feat: add migration scripts for job_log table creation and cloud_account table removal

---
+++ jc-commander/migrations/2025-03-12-063310_init_posts/up.sql
@@ -0,0 +1,18 @@
+-- Your SQL goes here
+-- create job_log table
+DROP TABLE IF EXISTS `job_log`;
+
+CREATE TABLE `job_log` (
+    `id` VARCHAR(255) NOT NULL COMMENT "job id",
+    `type` VARCHAR(255) NOT NULL COMMENT "task type",
+    `worker` VARCHAR(255) COMMENT "worker name",
+    `status` SMALLINT UNSIGNED NOT NULL COMMENT "status",
+    `params` VARCHAR(255) NOT NULL DEFAULT '{}' COMMENT "task params",
+    `create_time` DATETIME NULL COMMENT "task create time",
+    `finish_time` DATETIME NULL COMMENT "task finish time",
+    `update_time` DATETIME NULL COMMENT "log update time",
+    `comment` VARCHAR(255) NULL DEFAULT NULL COMMENT "task comment",
+    PRIMARY KEY (`id`),
+    KEY `type` (`type`),
+    KEY `worker` (`worker`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
