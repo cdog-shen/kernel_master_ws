70ec4b95676884a980288b67e6f23ed24999fa7c
cdog-shen <shencdog@outlook.com>
2025-03-17 02:12:22 +0800

    add: Implement log update function for JSON-RPC service

---
+++ jc-worker/src/service/json_rpc.rs
@@ -0,0 +1,51 @@
+use chrono::Local;
+
+use share_lib::data_structure::{MailManErr, MailManOk};
+
+use crate::config::worker;
+
+pub fn update_log<'a>(
+    auth: String,
+    uuid: String,
+    status: u8,
+    result: String,
+) -> Result<MailManOk<'a, String>, MailManErr<'a>> {
+    let commander_url = format!(
+        "http://{}/api/log/update",
+        worker::GLOBAL_CONFIG.read().unwrap().commander_addr
+    );
+    let worker_id = &worker::GLOBAL_CONFIG.read().unwrap().subsys_uuid;
+    let response = ureq::post(commander_url)
+        .header("Authorization", auth)
+        .header("content-type", "application/json")
+        .send(
+            serde_json::to_string(&serde_json::json!(		{
+                "id": uuid,
+                "worker": worker_id,
+                "status": status,
+                "result": result,
+                "finish_time": Local::now().naive_local().format("%Y-%m-%dT%H:%M:%S").to_string(),
+            }))
+            .unwrap(),
+        );
+
+    match response {
+        Ok(resp) => {
+            if resp.status() == 200 {
+                return Ok(MailManOk::new(
+                    200,
+                    "log update Done",
+                    Some(resp.into_body().read_to_string().unwrap()),
+                ));
+            } else {
+                return Err(MailManErr::new(
+                    500,
+                    "log update Failed",
+                    resp.into_body().read_to_string().unwrap(),
+                    1,
+                ));
+            }
+        }
+        Err(e) => return Err(MailManErr::new(500, "log update Failed", e, 1)),
+    }
+}
