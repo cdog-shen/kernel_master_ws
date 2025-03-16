5eb78e1a0ed80dd61381e8b8e574d250aba3aa9c
cdog-shen <shencdog@outlook.com>
2025-03-17 02:08:51 +0800

    add: Add unique ID and commander to job log for sync and async tasks

--- jc-commander/src/api/script_caller.rs
+++ jc-commander/src/api/script_caller.rs
@@ -1,13 +1,15 @@
 use std::sync::Arc;
 
 use actix_web::{web, HttpResponse};
+use chrono::{self, Local};
 use diesel::{
     r2d2::{ConnectionManager, Pool},
     MysqlConnection,
 };
 use serde_json::Value;
+use uuid::Uuid;
 
-use share_lib::data_structure::MailManOk;
+use share_lib::data_structure::{MailManErr, MailManOk};
 
 use crate::config::server;
 use crate::services::job_log;
@@ -23,9 +25,32 @@ pub async fn call_sync(
         "{}_sync",
         &server::GLOBAL_CONFIG.read().unwrap().mq_queue_prefix
     );
-    let payload = serde_json::to_vec(&req.into_inner()).unwrap();
 
-    let res = channel
+    let self_id = &server::GLOBAL_CONFIG.read().unwrap().subsys_uuid;
+    let uuid = Uuid::new_v4().to_string();
+    let mut req = req.into_inner();
+    req["id"] = serde_json::Value::String(uuid.clone());
+    req["commander"] = serde_json::Value::String(self_id.clone());
+    let payload = serde_json::to_vec(&req).unwrap();
+    let new_log_value = serde_json::json!(
+            {"id": uuid.clone(),
+            "script": req["script"],
+            "exec_type": "sync",
+            "commander": self_id,
+            "status": 1,
+            "params": req["params"].to_string(),
+            "result": "{}",
+            "create_time": Local::now().naive_local().format("%Y-%m-%dT%H:%M:%S").to_string(),
+            "comment": req["comment"],
+    });
+    let new_log = new_log_value.as_object().unwrap();
+
+    match job_log::new(new_log, &db_pool) {
+        Ok(_) => (),
+        Err(e) => return HttpResponse::InternalServerError().json(e),
+    }
+
+    match channel
         .basic_publish(
             "",
             &queue,
@@ -33,19 +58,65 @@ pub async fn call_sync(
             &payload,
             lapin::BasicProperties::default(),
         )
-        .await;
+        .await
+    {
+        Ok(res_data) => {
+            MailManOk::new(
+                200,
+                "Sync task send success",
+                Some(format!("{:?}", res_data)),
+            );
+        }
+        Err(e) => {
+            return HttpResponse::InternalServerError().json(MailManErr::new(
+                500,
+                "Task sending Failed",
+                e,
+                1,
+            ))
+        }
+    }
 
-    match res {
-        Ok(res_data) => HttpResponse::Ok().json(MailManOk::new(
-            200,
-            "Sync task sending success",
-            Some(format!("{:?}", res_data)),
-        )),
-        Err(err_data) => HttpResponse::InternalServerError().json(err_data.to_string()),
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+
+        match job_log::get_by_id(uuid.clone(), &db_pool) {
+            Ok(MailManOk {
+                code: _,
+                key: _,
+                data: job_log_res,
+            }) => match job_log_res {
+                Some(job_log) => {
+                    if job_log.status == 0 {
+                        return HttpResponse::InternalServerError().json(MailManErr::new(
+                            500,
+                            "Job execute Error",
+                            format!("{:?}", job_log),
+                            1,
+                        ));
+                    } else if job_log.status == 2 {
+                        return HttpResponse::Ok().json(MailManOk::new(
+                            200,
+                            "Sync task called success",
+                            Some(job_log),
+                        ));
+                    }
+                }
+                None => {
+                    return HttpResponse::InternalServerError().json(MailManErr::new(
+                        500,
+                        "no job found",
+                        uuid,
+                        1,
+                    ))
+                }
+            },
+            Err(e) => return HttpResponse::InternalServerError().json(e),
+        }
     }
 }
 
-// send sync task to message queue
+// send async task to message queue
 pub async fn call_async(
     req: web::Json<Value>,
     db_pool: web::Data<Pool<ConnectionManager<MysqlConnection>>>,
@@ -56,9 +127,32 @@ pub async fn call_async(
         "{}_async",
         &server::GLOBAL_CONFIG.read().unwrap().mq_queue_prefix
     );
-    let payload = serde_json::to_vec(&req.into_inner()).unwrap();
 
-    let res = channel
+    let self_id = &server::GLOBAL_CONFIG.read().unwrap().subsys_uuid;
+    let uuid = Uuid::new_v4().to_string();
+    let mut req = req.into_inner();
+    req["id"] = serde_json::Value::String(uuid.clone());
+    req["commander"] = serde_json::Value::String(self_id.clone());
+    let payload = serde_json::to_vec(&req).unwrap();
+    let new_log_value = serde_json::json!(
+            {"id": uuid.clone(),
+            "script": req["script"],
+            "exec_type": "async",
+            "commander": self_id,
+            "status": 1,
+            "params": req["params"].to_string(),
+            "result": "{}",
+            "create_time": Local::now().naive_local().format("%Y-%m-%dT%H:%M:%S").to_string(),
+            "comment": req["comment"],
+    });
+    let new_log = new_log_value.as_object().unwrap();
+
+    match job_log::new(new_log, &db_pool) {
+        Ok(_) => (),
+        Err(e) => return HttpResponse::InternalServerError().json(e),
+    }
+
+    match channel
         .basic_publish(
             "",
             &queue,
@@ -66,14 +160,22 @@ pub async fn call_async(
             &payload,
             lapin::BasicProperties::default(),
         )
-        .await;
-
-    match res {
-        Ok(res_data) => HttpResponse::Ok().json(MailManOk::new(
-            200,
-            "Sync task sending success",
-            Some(format!("{:?}", res_data)),
-        )),
-        Err(err_data) => HttpResponse::InternalServerError().json(err_data.to_string()),
+        .await
+    {
+        Ok(res_data) => {
+            return HttpResponse::Ok().json(MailManOk::new(
+                200,
+                "Async task send success",
+                Some(format!("{:?}", res_data)),
+            ))
+        }
+        Err(e) => {
+            return HttpResponse::InternalServerError().json(MailManErr::new(
+                500,
+                "Task sending Failed",
+                e,
+                1,
+            ))
+        }
     }
 }
