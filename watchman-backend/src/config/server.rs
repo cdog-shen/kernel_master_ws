73197b02d87d367220415d38cb270a76de6bb3a1
cdog-shen <shencdog@outlook.com>
2025-03-19 04:09:41 +0800

    add: Update the configuration structure by adding an allowed origins list and fixing the configuration file name.

--- watchman-backend/src/config/server.rs
+++ watchman-backend/src/config/server.rs
@@ -13,17 +13,18 @@ pub struct AllConfigs {
     pub log_path: String,
     pub log_level: String,
 
-    pub db_str: String,
-
     pub listen_addr: String,
     pub listen_port: u16,
-
+    pub allowed_origin_list: Vec<String>,
+    
     pub pub_key_path: String,
     pub pri_key_path: String,
     pub secret_key_path: String,
-
+    
     pub authenticate_bypass: Vec<String>,
     pub permit_bypass: Vec<String>,
+
+    pub db_str: String,
 }
 
 fn get_string_from_config(config: &Map<String, Value>, path: &[&str]) -> String {
@@ -38,14 +39,15 @@ impl AllConfigs {
         Self {
             log_path: String::new(),
             log_level: String::new(),
-            db_str: String::new(),
             listen_addr: String::new(),
             listen_port: 8000,
+            allowed_origin_list: vec![],
             pub_key_path: String::new(),
             pri_key_path: String::new(),
             secret_key_path: String::new(),
             authenticate_bypass: vec![],
             permit_bypass: vec![],
+            db_str: String::new(),
         }
     }
 
@@ -55,12 +57,17 @@ impl AllConfigs {
             Err(e) => return Err(e),
         };
 
-        println!("{:?}", config["server_config"]["log_path"]);
-
         self.log_path = get_string_from_config(&config, &["server_config", "log_path"]);
-        self.db_str = get_string_from_config(&config, &["db_config", "db_str"]);
         self.listen_addr = get_string_from_config(&config, &["server_config", "listen_addr"]);
         self.listen_port = config["server_config"]["listen_port"].as_u64().unwrap() as u16;
+        self.allowed_origin_list = match &config["server_config"]["allowed_origin_list"] {
+            Value::Array(vec) => vec
+                .iter()
+                .filter_map(|item| item.as_str())
+                .map(|item| item.to_string())
+                .collect(),
+            _ => vec![],
+        };
         self.pub_key_path = get_string_from_config(&config, &["server_config", "pub_key_path"]);
         self.pri_key_path = get_string_from_config(&config, &["server_config", "pri_key_path"]);
         self.secret_key_path =
@@ -81,6 +88,7 @@ impl AllConfigs {
                 .collect(),
             _ => vec![],
         };
+        self.db_str = get_string_from_config(&config, &["db_config", "db_str"]);
 
         Ok(0)
     }
@@ -89,7 +97,7 @@ impl AllConfigs {
 pub static CONFIG_FILE_HANDLE: Lazy<Mutex<File>> = Lazy::new(|| {
     let path = std::env::current_dir()
         .expect("Unable to get workspace path")
-        .join("watchman_server.cfg");
+        .join("watchman_backend.cfg");
     let file = File::open(&path).expect("Unable to open config file");
     Mutex::new(file)
 });
@@ -107,30 +115,3 @@ pub static SECRET_KEY: Lazy<RwLock<String>> = Lazy::new(|| {
         }
     })
 });
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_read_config_success() {
-        let config: Result<Map<String, Value>, share_lib::data_structure::MailManErr<'_>> =
-            read_config(&mut *CONFIG_FILE_HANDLE.lock().unwrap());
-
-        // println!("{:?}", config);
-        assert!(config.is_ok());
-
-        let config = config.unwrap();
-
-        println!("{:?}", config);
-        assert_eq!(config["server_config"]["listen_port"], 8000);
-
-        let config2 = read_config(&mut *CONFIG_FILE_HANDLE.lock().unwrap());
-
-        assert!(config2.is_ok());
-
-        let config2 = config2.unwrap();
-        println!("{:?}", config2);
-        assert_eq!(config2["server_config"]["listen_port"], 8000);
-    }
-}
