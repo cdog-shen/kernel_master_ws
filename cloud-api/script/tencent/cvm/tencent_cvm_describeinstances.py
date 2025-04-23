bf0ccc1566202b1b452d04b7eaf1b3255d1fa185
cdog-shen <shencdog@outlook.com>
2025-04-23 20:36:37 +0800

    add: tencent API CVM DescribeInstances

---
+++ cloud-api/script/tencent/cvm/tencent_cvm_describeinstances.py
@@ -0,0 +1,55 @@
+# 查询实例列表
+# 参数列表
+# 1. ak: SecretID
+# 2. sk: SecretKEY
+# 3. region: 地区
+# 4. params: 请求参数
+# 5. endpoint: 接口地址 (暂时固定, 不影响返回结果)
+# param示例:
+# {
+#     "InstanceIds": [
+#         "1111111"
+#     ],
+#     "Filters": [
+#         {
+#             "Name": "1111111",
+#             "Values": [
+#                 "11111111"
+#             ]
+#         }
+#     ]
+# }
+
+import json
+import sys
+import os
+from package_import import ApiClient_CVM
+
+FILE_NAME = os.path.basename(__file__)
+
+
+try:
+    if len(sys.argv) != 5:
+        print(f"Usage: python {FILE_NAME} <AK> <SK> <region> <params>")
+        sys.exit(1)
+
+    AK = sys.argv[1]
+    SK = sys.argv[2]
+    region = sys.argv[3]
+    params = json.loads(sys.argv[4])
+    endpoint = "cvm.tencentcloudapi.com"
+
+    client = ApiClient_CVM(AK, SK, region, endpoint)
+
+    req = client.ModelsHandler().DescribeInstancesRequest()
+    req.from_json_string(jsonStr=json.dumps(params))
+
+    resp = client.ClientHandler().DescribeInstances(req)
+
+    print(resp.to_json_string(), end="")
+
+
+except Exception as err:
+    import traceback
+
+    print(traceback.format_exc())
