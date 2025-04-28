29511980469a6d72a26ebc1530e8d6b1c4d69681
cdog-shen <shencdog@outlook.com>
2025-04-28 14:27:33 +0800

    add: tokio RT for timewheel thread

--- jc-commander/src/main.rs
+++ jc-commander/src/main.rs
@@ -1,7 +1,7 @@
 // std import
 use std::default::Default;
 use std::io;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 // rt import
 use actix_cors::Cors;
 use actix_web::dev::Service;
@@ -26,7 +26,7 @@ mod config;
 mod middleware;
 mod models;
 mod services;
-// mod utils;
+mod util;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
@@ -63,6 +63,30 @@ async fn main() -> io::Result<()> {
         .expect("Failed to connect to MQ");
     let mq_pool: Arc<Connection> = Arc::new(mq_manager);
 
+    // initialize the scheduled task scheduler and start Ticking
+    let time_wheel = Arc::new(util::scheduler::TimeWheel::new(
+        db_pool.clone(),
+        mq_pool.clone(),
+    ));
+    let flush_flag = Arc::new(Mutex::new(false));
+    let ffc = flush_flag.clone();
+    let _ = time_wheel.reload_from_db();
+    tokio::spawn(async move {
+        log::info!("TimeWheel thread launched. Ticking...");
+        let mut interval = tokio::time::interval(std::time::Duration::from_secs(1));
+        loop {
+            log::info!("ffc: {}", ffc.lock().unwrap());
+            if *ffc.lock().unwrap() {
+                *ffc.lock().unwrap() = false;
+                let _ = time_wheel.reload_from_db();
+            }
+            interval.tick().await;
+            if time_wheel.tick() {
+                *ffc.lock().unwrap() = true;
+            }
+        }
+    });
+
     // setting listen addr
     let app_url = format!(
         "{}:{}",
@@ -70,10 +94,10 @@ async fn main() -> io::Result<()> {
         &server::GLOBAL_CONFIG.read().unwrap().listen_port
     );
     let allowed_origin_list = server::GLOBAL_CONFIG
-    .read()
-    .unwrap()
-    .allowed_origin_list
-    .clone();
+        .read()
+        .unwrap()
+        .allowed_origin_list
+        .clone();
 
     HttpServer::new(move || {
         App::new()
@@ -93,6 +117,7 @@ async fn main() -> io::Result<()> {
             )
             .app_data(web::Data::new(db_pool.clone()))
             .app_data(web::Data::new(mq_pool.clone()))
+            .app_data(web::Data::from(flush_flag.clone()))
             // wrap default logger
             .wrap(actix_web::middleware::Logger::default())
             // Comment this line if you want to integrate with yew-address-book-frontend
