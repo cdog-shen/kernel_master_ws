1210e071633ec946f7e68c921e753c621ad4ff80
cdog-shen <shencdog@outlook.com>
2025-06-23 20:13:42 +0800

    feat: generate new model

--- cmdb-backend/src/models/schema.rs
+++ cmdb-backend/src/models/schema.rs
@@ -1,10 +1,33 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    lighthouse_instence (id) {
+    cloudserver_instance (id) {
         id -> Unsigned<Bigint>,
         #[max_length = 255]
-        project -> Varchar,
+        provider -> Varchar,
+        #[max_length = 255]
+        zone -> Varchar,
+        #[max_length = 255]
+        instance_id -> Varchar,
+        #[max_length = 255]
+        instance_name -> Varchar,
+        #[max_length = 255]
+        plantform -> Varchar,
+        #[max_length = 255]
+        status -> Varchar,
+        #[max_length = 255]
+        tag -> Varchar,
+        #[max_length = 255]
+        private_ip -> Varchar,
+        full_info -> Nullable<Text>,
+        attach_info -> Nullable<Text>,
+        update_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
+    lighthouse_instance (id) {
+        id -> Unsigned<Bigint>,
         #[max_length = 255]
         cloud_name -> Varchar,
         #[max_length = 255]
@@ -18,29 +41,21 @@ diesel::table! {
         #[max_length = 255]
         wip -> Nullable<Varchar>,
         #[max_length = 255]
-        nip -> Varchar,
-        #[max_length = 255]
         vpc_id -> Nullable<Varchar>,
-        #[max_length = 255]
-        subnet_id -> Nullable<Varchar>,
-        #[max_length = 255]
-        instance_type -> Nullable<Varchar>,
-        #[max_length = 255]
-        internet_charge_type -> Varchar,
         #[max_length = 255]
         status -> Varchar,
         #[max_length = 255]
         os_name -> Varchar,
         #[max_length = 255]
         os_type -> Varchar,
-        #[max_length = 255]
-        image_id -> Varchar,
-        #[max_length = 255]
-        bandwidth -> Varchar,
-        #[max_length = 255]
-        cloud_account -> Varchar,
-        is_link_server -> Bool,
         create_at -> Nullable<Timestamp>,
         update_at -> Nullable<Timestamp>,
+        full_info -> Nullable<Text>,
+        attach_info -> Nullable<Text>,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    cloudserver_instance,
+    lighthouse_instance,
+);
