a2c0df67a6cbb2a5faa74aa300301c9a5ba57ea3
cdog-shen <shencdog@outlook.com>
2025-06-24 19:13:27 +0800

    fix: Fixed quotation mark errors in string formatting

--- jc-worker/script/util/auth.py
+++ jc-worker/script/util/auth.py
@@ -28,7 +28,7 @@ def login():
                 continue
 
         elif resp_json.get("code", 500) == 200:
-            jwt_str = f"{resp_json.get("data").get("token_type")} {resp_json.get("data").get("token")}"
+            jwt_str = f"{resp_json.get('data').get('token_type')} {resp_json.get('data').get('token')}"
             os.environ["WATCHMAN_JWT"] = jwt_str
             JWT = jwt_str
             break
