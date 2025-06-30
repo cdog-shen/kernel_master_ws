b9009e2de1be041ffcac27f6d12f395e7187517d
cdog-shen <shencdog@outlook.com>
2025-06-30 17:20:54 +0800

    build: a simple build script

---
+++ build/build_all_ws.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DIRS="cloud-api cmdb-backend jc-commander jc-worker share-lib watchman-backend"
+
+for dir in $DIRS; do
+    if [ ! -d "$dir" ]; then
+        echo "Directory $dir does not exist."
+        echo "Usage ./build/build_all_ws.sh"
+        exit 1
+    fi
+done
+
+mkdir -p ./target
+
+for dir in $DIRS; do
+    cd $dir && cargo build --release && cd -
+done
+
+exit 0
