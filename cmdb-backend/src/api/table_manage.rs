805814af9d3dc62922170641a429c4092cf2e7a0
cdog-shen <shencdog@outlook.com>
2025-07-08 17:51:06 +0800

    fix: fix name error

--- cmdb-backend/src/api/table_manage.rs
+++ cmdb-backend/src/api/table_manage.rs
@@ -38,7 +38,7 @@ pub async fn get_table(
             }
         }
         "logservice_topic" => {
-            let result = logserver::topic_service::get_all(&query, &pool);
+            let result = logservice::topic_service::get_all(&query, &pool);
             match result {
                 Ok(data) => Ok(HttpResponse::Ok().json(data)),
                 Err(err) => Ok(HttpResponse::BadRequest().json(err)),
@@ -108,7 +108,7 @@ pub async fn update_table(
             }
         }
         "logservice_topic" => {
-            let result = logserver::topic_service::update_table(&update, &pool);
+            let result = logservice::topic_service::update_table(&update, &pool);
             match result {
                 Ok(data) => Ok(HttpResponse::Ok().json(data)),
                 Err(err) => Ok(HttpResponse::BadRequest().json(err)),
@@ -143,7 +143,7 @@ pub async fn delete_table(
             }
         }
         "logservice_topic" => {
-            let result = logserver::topic_service::delete_table(&delete, &pool);
+            let result = logservice::topic_service::delete_table(&delete, &pool);
             match result {
                 Ok(data) => Ok(HttpResponse::Ok().json(data)),
                 Err(err) => Ok(HttpResponse::BadRequest().json(err)),
--- cmdb-backend/src/services/mod.rs
+++ cmdb-backend/src/services/mod.rs
@@ -1,3 +1,3 @@
 pub mod cloudserver;
 pub mod lighthouse;
-pub mod logserver;
+pub mod logservice;
