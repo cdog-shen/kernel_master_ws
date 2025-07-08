f6c4bd8183103b4c1c2a958b6741e3386b492875
cdog-shen <shencdog@outlook.com>
2025-07-08 13:49:39 +0800

    feat: add logservice module and define logservice_topic schema

--- cmdb-backend/src/models/mod.rs
+++ cmdb-backend/src/models/mod.rs
@@ -1,5 +1,6 @@
 // table struct
 pub mod cloudserver;
 pub mod lighthouse;
+pub mod logservice;
 // table schema
 pub mod schema;
--- cmdb-backend/src/models/schema.rs
+++ cmdb-backend/src/models/schema.rs
@@ -26,51 +26,6 @@ diesel::table! {
 }
 
 diesel::table! {
-    light_ecs_table (id) {
-        id -> Unsigned<Bigint>,
-        #[max_length = 255]
-        project -> Varchar,
-        #[max_length = 255]
-        cloud_name -> Varchar,
-        #[max_length = 255]
-        region -> Varchar,
-        #[max_length = 255]
-        zone -> Varchar,
-        #[max_length = 255]
-        instance_id -> Varchar,
-        #[max_length = 255]
-        instance_name -> Varchar,
-        #[max_length = 255]
-        wip -> Nullable<Varchar>,
-        #[max_length = 255]
-        nip -> Varchar,
-        #[max_length = 255]
-        vpc_id -> Nullable<Varchar>,
-        #[max_length = 255]
-        subnet_id -> Nullable<Varchar>,
-        #[max_length = 255]
-        instance_type -> Nullable<Varchar>,
-        #[max_length = 255]
-        internet_charge_type -> Varchar,
-        #[max_length = 255]
-        status -> Varchar,
-        #[max_length = 255]
-        os_name -> Varchar,
-        #[max_length = 255]
-        os_type -> Varchar,
-        #[max_length = 255]
-        image_id -> Varchar,
-        #[max_length = 255]
-        bandwidth -> Varchar,
-        #[max_length = 255]
-        cloud_account -> Varchar,
-        is_link_server -> Bool,
-        create_at -> Nullable<Timestamp>,
-        update_at -> Nullable<Timestamp>,
-    }
-}
-
-diesel::table! {
     lighthouse_instance (id) {
         id -> Unsigned<Bigint>,
         #[max_length = 255]
@@ -100,8 +55,32 @@ diesel::table! {
     }
 }
 
+diesel::table! {
+    logservice_topic (id) {
+        id -> Unsigned<Bigint>,
+        #[max_length = 255]
+        provider -> Varchar,
+        #[max_length = 255]
+        set_id -> Varchar,
+        #[max_length = 255]
+        topic_id -> Varchar,
+        #[max_length = 255]
+        topic_name -> Varchar,
+        #[max_length = 255]
+        status -> Varchar,
+        hot_period -> Unsigned<Integer>,
+        period -> Unsigned<Integer>,
+        index -> Tinyint,
+        full_info -> Nullable<Text>,
+        attach_info -> Nullable<Text>,
+        update_at -> Nullable<Timestamp>,
+        #[max_length = 255]
+        describes -> Varchar,
+    }
+}
+
 diesel::allow_tables_to_appear_in_same_query!(
     cloudserver_instance,
-    light_ecs_table,
     lighthouse_instance,
+    logservice_topic,
 );
