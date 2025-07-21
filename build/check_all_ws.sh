6ec495c69acc94ff42f2bd8bbff8187a086cfbdd
cdog-shen <shencdog@outlook.com>
2025-07-21 17:22:41 +0800

    build: check T script

---
+++ build/check_all_ws.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DIRS="cloud-api cmdb-backend jc-commander jc-worker share-lib watchman-backend"
+
+for dir in $DIRS; do
+    if [ ! -d "$dir" ]; then
+        echo "Directory $dir does not exist."
+        echo "Usage ./build/build_all_ws.sh"
+        exit 1
+    fi
+done
+
+mkdir -p ./target
+
+for dir in $DIRS; do
+    cd $dir && cargo check
+    cp -f target/release/$dir ../target/
+    cd -
+done
+
+exit 0
